8c1186be82b4298ba4888763345a2a648a0dd884
Megan Sullivan <meganesulli@gmail.com>
2018-07-12 13:49:32 -0700

    Update Factorial class to make all Factorial tests pass.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,12 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i) throws IllegalArgumentException {
+        if (i < 0) throw new IllegalArgumentException("i is less than 0");
+        int factorial = 1;
+        for (int num = 1; num <= i; num++) {
+            factorial *= num;
+        }
+        return factorial;
     }
 }
